305e55b335ece8df047010a46f23e2d964d27f11
Brandon Bakus <brandonbakus@gmail.com>
2025-01-08 15:31:22 -0500

    proud of this one

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+
+
+function findMatching(array, driverName){
+    return array.filter(driver => {
+        return driver.toLowerCase() === driverName.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, string){
+    return array.filter((person) => {
+        return person[0] === string[0]
+    })
+}
+
+function matchName(array, string){
+    return array.filter(object => {
+        for(const key in object){
+            return object[key] === string
+        }
+
+        }
+    )
+}
+
+
